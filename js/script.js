956b0600cf3eace7a4ac825d2e555301c0b9e28b
asamatokhin <asamatokhin@gmail.com>
2019-03-30 19:28:52 +0300

    подготовил проект к стилизации

--- js/script.js
+++ js/script.js
@@ -16,4 +16,19 @@ $(document).ready(function () {
             $(this).addClass('active');
         }
     })
+    var btnUp = $('.up');
+    btnUp.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            btnUp.fadeIn();
+        } else {
+            btnUp.fadeOut();
+        }
+    })
+    btnUp.click(function () {
+        $('body,html').animate({
+            scrollTop:0
+        }, 500);
+        return false
+    });
 })
